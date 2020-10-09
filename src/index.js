b67e41b52e4bfde8b83d79dc472ac373219c4ca4
DavePlayer <daveplayer2001@gmail.com>
2020-10-09 17:39:45 +0200

    finished bot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log('works')
+import discord from 'discord.js'
+import dotenv from 'dotenv'
+import node_fetch from 'node-fetch'
+const fetch = node_fetch
+dotenv.config()
+
+const Client = new discord.Client()
+
+Client.on('ready', async () => {
+    console.log('bot włączony')
+    let isStreaming = false
+    let didInform = false
+    const clearInform = async () => {
+        await setTimeout(() => {
+            didInform = false
+        }, 1200 * 12)
+    }
+    await setInterval( async () => {
+        const json = await fetch(`https://api.twitch.tv/kraken/streams/${process.env.TWITCH_CHANNEL_ID}?client_id=${process.env.TWITCH_CLIENT_ID}&token=${process.env.TWITCH_TOKEN}&api_version=5`)
+        const StreamData = await json.json()
+        const embeded = new discord.MessageEmbed()
+        .setTitle(`${StreamData.stream.channel.game} : ${StreamData.stream.channel.status}`).setColor(0xFA3C87)    
+        .setURL(`${StreamData.stream.channel.url}`)
+        .setImage(`${StreamData.stream.preview.large}`);
+        // embeded.setTitle(`@everyone ${process.env.TWITCH_USERNAME} aka ciota teraz streamuje`)
+        // // embeded.url(`https://www.twitch.tv/mihalx`)
+        // embeded.video = {url: 'https://www.twitch.tv/mihalx', height: 17, width: 17}
+        // // embeded.video({url: 'https://www.twitch.tv/mihalx', height: 17, width: 17})
+        if(StreamData.stream && isStreaming == false){
+            console.log(StreamData.stream)
+            isStreaming = true
+            Client.channels.cache.get('764121237105934366').send(`@everyone ${process.env.TWITCH_USERNAME} teraz streamuje`, embeded)
+        } else if(!StreamData.stream && isStreaming == true) {
+            isStreaming = false
+            //clearInform()
+        }
+    }, 5000)
+})
+
+console.log('działa')
+
+Client.login(process.env.TOKEN)
