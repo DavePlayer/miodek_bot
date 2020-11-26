8db6608fc532c7cf195e8b4d7cf18d9d983254ba
DavePlayer <davepalyer2001@gmail.com>
2020-11-26 19:28:52 +0100

    added functionality of saving user roles to json file on server

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import discord from 'discord.js'
 import dotenv from 'dotenv'
 import node_fetch from 'node-fetch'
 const fetch = node_fetch
+import fs from 'fs'
 dotenv.config()
 
 const Client = new discord.Client()
+const Guild = new discord.Guild(Client)
 
 Client.on('ready', async () => {
     console.log('bot włączony')
@@ -30,12 +32,55 @@ Client.on('ready', async () => {
             .setTitle(`${StreamData.stream.channel.game} : ${StreamData.stream.channel.status}`).setColor(0xFA3C87)    
             .setURL(`${StreamData.stream.channel.url}`)
             .setImage(`${StreamData.stream.preview.large}`);
-            Client.channels.cache.get('748643741874782318').send(`@everyone ${process.env.TWITCH_USERNAME} teraz streamuje`, embeded)
+            Client.channels.cache.get(process.env.DISCORD_CHANNEL).send(`@everyone ${process.env.TWITCH_USERNAME} teraz streamuje`, embeded)
         } else if(!StreamData.stream && isStreaming == true) {
             isStreaming = false
             //clearInform()
         }
     }, 1000 * 60)
+    Client.channels.cache.get(process.env.DISCORD_CHANNEL).send('dzialam')
+})
+
+Client.on('message', message => {
+    console.log(message.channel.id)
+    if(message.channel.id == process.env.DISCORD_COMMAND_CHANNEL && message.content.includes('BOT') ){
+        const regex = message.content.match(/BOT (.*)/)
+        if(regex != null){
+            const command = regex[1]
+            let users = []
+            let roles = []
+            switch(command){
+                case 'list users':
+                    const cache = Client.guilds.cache.get(process.env.DISCORD_SERVER_ID).members.cache.forEach( user => {
+                        users = [...users, user]
+                    } )
+                    // console.log(users)
+                    const chache2 = Client.guilds.cache.get(process.env.DISCORD_SERVER_ID).roles.cache.forEach(role => {
+                        roles = [...roles, {id: role.id, name: role.name}]
+                    })
+                    console.log(roles)
+                    console.log('--------------------------------------')
+                    console.log(users[0]._roles)
+                    let json = []
+                    users.map( user => {
+                        console.log(user.user)
+                        json = [...json, {name: user.user.username, clientId: user.user.id, roles: roles.filter( role => {
+                            console.log(role)
+                            if(user._roles.includes(role.id))
+                                return role.name
+                        } ).map(o => o.name), user: user._roles}]
+                    } )
+                    json = [...json, roles]
+                    console.log(json)
+                    const data = JSON.stringify(json)
+                    fs.writeFile('roles.json', data, err => {
+                        if(err)
+                            console.log(err)
+                    })
+            }
+        }
+    }
+
 })
 
 console.log('działa')
