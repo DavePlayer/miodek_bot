e0d20332383106fcfdafd6c9105abcd55a624e21
DavePlayer <davepalyer2001@gmail.com>
2020-11-26 21:55:58 +0100

    finished role savung functionality(due to lack of better hardware function is set to command)

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ dotenv.config()
 const Client = new discord.Client()
 const Guild = new discord.Guild(Client)
 
+
+const getFileJson = () => {
+    return JSON.parse(fs.readFileSync('./roles.json', 'utf-8'))
+}
+
+
 Client.on('ready', async () => {
     console.log('bot włączony')
     // Client.channels.cache.get('748643741874782318').send('Dzień dobry. Od dziś będę pomagał w śledz... znaczy się pomaganiu mihalxowi w wypisywaniu kiedy będzie streamować')
@@ -38,6 +44,7 @@ Client.on('ready', async () => {
             //clearInform()
         }
     }, 1000 * 60)
+
     Client.channels.cache.get(process.env.DISCORD_CHANNEL).send('dzialam')
 })
 
@@ -64,13 +71,14 @@ Client.on('message', message => {
                     let json = []
                     users.map( user => {
                         console.log(user.user)
+                        if(user._roles.length > 0)
                         json = [...json, {name: user.user.username, clientId: user.user.id, roles: roles.filter( role => {
                             console.log(role)
                             if(user._roles.includes(role.id))
                                 return role.name
                         } ).map(o => o.name), user: user._roles}]
                     } )
-                    json = [...json, roles]
+                    json = [...json, {roles}]
                     console.log(json)
                     const data = JSON.stringify(json)
                     fs.writeFile('roles.json', data, err => {
@@ -82,6 +90,33 @@ Client.on('message', message => {
     }
 
 })
+
+//Client.on('guildMemberUpdate', member => {
+// niby dziala na kazda zmiane roi, ale tez zmianie pseudonimu jak i usuniecie albo dodanie uzytkownika
+    //console.log(member.roles.find())
+//})
+
+Client.on('guildMemberAdd', member => {
+    console.log(member)
+    const welcomeMessages = [
+        `Świeże mięso na horyzoncie ${member.user.username}`,
+        `Popełnijmy razem przestępstwo podatkowe ${member.user.username}`,
+        `Welcome to the cum zone ${member.user.username}`,
+        `Brawo! Znalazłeś jeden z lepszych śmietników na tym świecie ${member.user.username}`,
+        `GOD IS DED!! GOD WEMEINS DED! AND WE AWD ${member.user.username} HAVE KIWWED HIM! HOW SHAWW WE COMFOWT OWSEWVES! THE MWUWDEWEWS OF AWWW MWUWDEWEWS! `,
+    ]
+    member.guild.channels.cache.get(process.env.DISCORD_CHANNEL).send(welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)])
+    // member.roles.add(member.guild.roles.cache.find(r => r.name == 'debil'))
+    const existingUsers = getFileJson()
+    console.log(existingUsers)
+    existingUsers.map( o => {
+        if(typeof o.clientId != 'undefined' && o.clientId)
+        if(o.clientId == member.user.id){
+            o.roles.map(role => member.roles.add(member.guild.roles.cache.find(r => r.name == role)))
+        }
+    } )
+});
+
 
 console.log('działa')
 
