a1c19e937fc1a093513c4d650d266c89fc765e26
DavePlayer <daveplayer2001@gmail.com>
2021-01-10 20:34:30 +0100

    fixed saving punished users error and set punishment role to env variable

--- src/rolePunichment.js
+++ src/rolePunichment.js
@@ -10,22 +10,22 @@ export const rolePunish = (Client, users, time) => {
     // }
     const json = JSON.parse(fs.readFileSync('./roles.json', 'utf-8'))
     const roles = json[json.length -1]
-
     const cache = Client.guilds.cache.get(process.env.DISCORD_SERVER_ID).members.cache.forEach( user => {
         if(users.has(user.user.id) ){
             if(punishedUsers.includes(user.user.username)) {
                 console.log(`${user.user.username} już nie żyż`)
                 return user
             } else {
-                user.roles.add(user.guild.roles.cache.find(r => r.name == "Nie żyż"))
+                user.roles.add(user.guild.roles.cache.find(r => r.name == process.env.PUNISHMENT_ROLE))
                 punishedUsers = [...punishedUsers, user.user.username]
                 fs.writeFileSync('./punishedUsers.json', JSON.stringify(punishedUsers))
                 setTimeout(async () => {
                     console.log('------------\n zwalnianie użytkownika \n')
                     user.roles.remove(user.guild.roles.cache.find(r => r.name == "Nie żyż"))
-                    fs.writeFileSync('./punishedUsers.json', JSON.stringify(punishedUsers.filter(o => {
+                    punishedUsers = punishedUsers.filter(o => {
                         if(o != user.user.username) return user
-                    }))) 
+                    })
+                    fs.writeFileSync('./punishedUsers.json', JSON.stringify(punishedUsers))
                     console.log(punishedUsers.filter(o => {
                         if(o != user.user.username) return user
                     }))
