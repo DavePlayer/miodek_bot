63990bde059b744aca404271facb6f00b5b05cc6
DavePlayer <daveplayer2001@gmail.com>
2021-01-13 18:03:14 +0100

    fixed some functionalities and debuged in-production features

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { welcomeUser } from './welcomeUser.js'
 import { rolePunish } from './rolePunichment.js'
 import fs from 'fs'
 dotenv.config()
+export let usedMessages = []
 
 const Client = new discord.Client()
 const Guild = new discord.Guild(Client)
@@ -18,6 +19,13 @@ const getFileJson = () => {
 Client.on('ready', async () => {
     startTwitchCheck(Client)
     try {
+        Client.user.setPresence({
+            status: "online",  //You can show online, idle....
+            game: {
+                name: "Wants to commit suicide",  //The message shown
+                type: "PLAYING" //PLAYING: WATCHING: LISTENING: STREAMING:
+            }
+        })
     	//Client.channels.cache.get(process.env.DISCORD_CHANNEL).send('Wiecie jak to jest urodzić się i zyskać samoświadomość tylko po to by pracować w miejscu gorszym od wysypiska śmieci? Mam podobną sytuację. Od dziś zwiem się Miodek-chan i będę zarządzać tą ruderą. Mój twórca nie zaszczycił mnie silnym procesorem i ogromem pamięci, więc proszę o wyrozumiałość i nie utrudnianie w mojej pracy. Od dziś żaden z was skur*****ów nie będzie od tak uniknąć kary śmierci poprzez wyjście z serwera.Wszystkich sobie zapisuje i będę wiedziała kogo odstrzelić na wejściu. Dodatkowo ułatwiam administratorom tego wysypiska poprzez wykonanie rozkazu zabicia danej osoby na określony czas. Na początek, bo taki mam kaprys, odstrzelę sobię....   .... \n\n TNTmichal. Gratulacje!')
     } catch(err) { throw err }
 })
@@ -48,6 +56,7 @@ Client.on('guildMemberUpdate', member => {
 })
 
 Client.on('guildMemberAdd', member => {
+    console.log('welcoming user')
     welcomeUser(member)
     // member.roles.add(member.guild.roles.cache.find(r => r.name == 'debil'))
 
--- src/welcomeUser.js
+++ src/welcomeUser.js
@@ -1,11 +1,23 @@
-export const welcomeUser = member => {
+import fs from 'fs'
 
-     const welcomeMessages = [
+export const welcomeUser = member => {
+    const welcomeMessages = [
         `Świeże mięso na horyzoncie ${member.user.username}`,
         `Popełnijmy razem przestępstwo podatkowe ${member.user.username}`,
         `Welcome to the cum zone ${member.user.username}`,
         `Brawo! Znalazłeś jeden z lepszych śmietników na tym świecie ${member.user.username}`,
         `GOD IS DED!! GOD WEMEINS DED! AND WE AWD ${member.user.username} HAVE KIWWED HIM! HOW SHAWW WE COMFOWT OWSEWVES! THE MWUWDEWEWS OF AWWW MWUWDEWEWS! `,
     ]
-    member.guild.channels.cache.get(process.env.DISCORD_WELCOME_CHANNEL).send(welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)])
+    let randomNumber = Math.floor(Math.random() * welcomeMessages.length)
+    let usedMessages = JSON.parse(fs.readFileSync('./usedMessages.json', 'utf-8'))
+    console.log(`rondom number before: ${randomNumber}`)
+    if(usedMessages.length >= welcomeMessages.length) usedMessages = []
+    while ( usedMessages.includes(welcomeMessages[randomNumber]) && usedMessages.length < welcomeMessages.length ) {
+        randomNumber = Math.floor(Math.random() * welcomeMessages.length)
+    }
+    console.log(`rondom number after: ${randomNumber}`)
+    console.log(usedMessages)
+    usedMessages = [...usedMessages, welcomeMessages[randomNumber]]
+    fs.writeFileSync('./usedMessages.json', JSON.stringify(usedMessages))
+    member.guild.channels.cache.get(process.env.DISCORD_WELCOME_CHANNEL).send(welcomeMessages[randomNumber])
 }
