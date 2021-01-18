f5c78538e4bb7436310f12064e4a4271260eb30d
DavePlayer <daveplayer2001@gmail.com>
2021-01-18 19:27:58 +0100

    not for future me "http can't send big numbers if not string"

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ app.post("/send", (req, res) => {
                 break;
             default:
                 Client.channels.cache
-                    .get(parseInt(req.body.channelId))
+                    .get(req.body.channelId)
                     .send(req.body.message);
                 break;
         }
