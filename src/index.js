7cb05c283e1a6c878b94f2c0e1626d364566da8d
DavePlayer <daveplayer2001@gmail.com>
2021-01-22 16:58:58 +0100

    added user punishment handler for users who were punished but tried to exit server

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import discord from "discord.js";
-import express from "express";
-import "@babel/polyfill";
-import dotenv from "dotenv";
-import { makeUserList } from "./userList.js";
-import { startTwitchCheck } from "./startTwitchCheck.js";
-import { welcomeUser } from "./welcomeUser.js";
-import { rolePunish } from "./rolePunichment.js";
-import fs from "fs";
-dotenv.config();
-export let usedMessages = [];
+import discord from "discord.js"
+import express from "express"
+import "@babel/polyfill"
+import dotenv from "dotenv"
+import { makeUserList } from "./userList.js"
+import { startTwitchCheck } from "./startTwitchCheck.js"
+import { welcomeUser } from "./welcomeUser.js"
+import { rolePunish } from "./rolePunichment.js"
+import fs from "fs"
+dotenv.config()
+export let usedMessages = []
 
-const Client = new discord.Client();
-const Guild = new discord.Guild(Client);
-const app = express();
-app.use(express.json());
+const Client = new discord.Client()
+const Guild = new discord.Guild(Client)
+const app = express()
+app.use(express.json())
 
 const getFileJson = () => {
-    return JSON.parse(fs.readFileSync("./roles.json", "utf-8"));
-};
+    return JSON.parse(fs.readFileSync("./roles.json", "utf-8"))
+}
 
 app.post("/send", (req, res) => {
-    console.log(req.body);
+    console.log(req.body)
     // http post body structure
     //{
     //"type": "sendMessage",
@@ -28,39 +28,27 @@ app.post("/send", (req, res) => {
     //"channelId": 1234567890,
     //"message": "just message"
     //}
-    if (
-        req.body.type == "sendMessage" &&
-        req.body.channel.length > 0 &&
-        req.body.channelId
-    ) {
+    if (req.body.type == "sendMessage" && req.body.channel.length > 0 && req.body.channelId) {
         switch (req.body.channel) {
             case "bot":
-                Client.channels.cache
-                    .get(process.env.DISCORD_COMMAND_CHANNEL)
-                    .send(req.body.message);
-                break;
+                Client.channels.cache.get(process.env.DISCORD_COMMAND_CHANNEL).send(req.body.message)
+                break
             case "info-social":
-                Client.channels.cache
-                    .get(process.env.DISCORD_CHANNEL)
-                    .send(req.body.message);
-                break;
+                Client.channels.cache.get(process.env.DISCORD_CHANNEL).send(req.body.message)
+                break
             case "ogolny":
-                Client.channels.cache
-                    .get(process.env.DISCORD_MAIN_CHANNEL)
-                    .send(req.body.message);
-                break;
+                Client.channels.cache.get(process.env.DISCORD_MAIN_CHANNEL).send(req.body.message)
+                break
             default:
-                Client.channels.cache
-                    .get(req.body.channelId)
-                    .send(req.body.message);
-                break;
+                Client.channels.cache.get(req.body.channelId).send(req.body.message)
+                break
         }
     }
-    res.send({ stsus: "working" });
-});
+    res.send({ stsus: "working" })
+})
 
 Client.on("ready", async () => {
-    startTwitchCheck(Client);
+    startTwitchCheck(Client)
     try {
         Client.user.setPresence({
             status: "online", //You can show online, idle....
@@ -68,64 +56,53 @@ Client.on("ready", async () => {
                 name: "Wants to commit suicide", //The message shown
                 type: "PLAYING", //PLAYING: WATCHING: LISTENING: STREAMING:
             },
-        });
+        })
         //Client.channels.cache.get(process.env.DISCORD_CHANNEL).send('Wiecie jak to jest urodzić się i zyskać samoświadomość tylko po to by pracować w miejscu gorszym od wysypiska śmieci? Mam podobną sytuację. Od dziś zwiem się Miodek-chan i będę zarządzać tą ruderą. Mój twórca nie zaszczycił mnie silnym procesorem i ogromem pamięci, więc proszę o wyrozumiałość i nie utrudnianie w mojej pracy. Od dziś żaden z was skur*****ów nie będzie od tak uniknąć kary śmierci poprzez wyjście z serwera.Wszystkich sobie zapisuje i będę wiedziała kogo odstrzelić na wejściu. Dodatkowo ułatwiam administratorom tego wysypiska poprzez wykonanie rozkazu zabicia danej osoby na określony czas. Na początek, bo taki mam kaprys, odstrzelę sobię....   .... \n\n TNTmichal. Gratulacje!')
     } catch (err) {
-        throw err;
+        throw err
     }
-});
+})
 
 Client.on("message", (message) => {
-    console.log(message.channel.id);
-    if (
-        message.channel.id == process.env.DISCORD_COMMAND_CHANNEL &&
-        message.content.includes("BOT")
-    ) {
-        const regex = message.content.match(/BOT (.*)/);
+    console.log(message.channel.id)
+    if (message.channel.id == process.env.DISCORD_COMMAND_CHANNEL && message.content.includes("BOT")) {
+        const regex = message.content.match(/BOT (.*)/)
         if (regex != null) {
-            const command = regex[1];
+            const command = regex[1]
             switch (true) {
                 case command.includes("save users"):
-                    makeUserList(message, Client);
-                    break;
+                    makeUserList(message, Client)
+                    break
                 case command.includes("punish"):
-                    const time = command.split(" ");
-                    rolePunish(
-                        Client,
-                        message.mentions.users,
-                        time[time.length - 1]
-                    );
-                    break;
+                    const time = command.split(" ")
+                    rolePunish(Client, message.mentions.users, time[time.length - 1])
+                    break
             }
         }
     }
-});
+})
 
 Client.on("guildMemberUpdate", (member) => {
     // niby dziala na kazda zmiane roi, ale tez zmianie pseudonimu jak i usuniecie albo dodanie uzytkownika
-    makeUserList(member, Client);
-});
+    makeUserList(member, Client)
+})
 
 Client.on("guildMemberAdd", (member) => {
-    console.log("welcoming user");
-    welcomeUser(member);
+    console.log("welcoming user")
+    welcomeUser(member)
     // member.roles.add(member.guild.roles.cache.find(r => r.name == 'debil'))
 
     // user roles validation and assignment
     getFileJson().map((o) => {
         if (typeof o.clientId != "undefined" && o.clientId)
             if (o.clientId == member.user.id) {
-                o.roles.map((role) =>
-                    member.roles.add(
-                        member.guild.roles.cache.find((r) => r.name == role)
-                    )
-                );
+                o.roles.map((role) => member.roles.add(member.guild.roles.cache.find((r) => r.name == role)))
             }
-    });
-});
+    })
+})
 
-console.log("działa");
+console.log("działa")
 
-Client.login(process.env.TOKEN);
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening on ${port}`));
+Client.login(process.env.TOKEN)
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`listening on ${port}`))
--- src/rolePunichment.js
+++ src/rolePunichment.js
@@ -1,71 +1,61 @@
-import fs from "fs";
+import fs from "fs"
 
 export const rolePunish = (Client, users, time) => {
-    console.log("punish him", users, time);
+    console.log("punish him", users, time)
 
-    let punishedUsers = JSON.parse(
-        fs.readFileSync("./punishedUsers.json", "utf-8")
-    );
+    let punishedUsers = JSON.parse(fs.readFileSync("./punishedUsers.json", "utf-8"))
     //if(punishedUsers.includes(userName)){
     //console.log('user already punished')
     //return false
     // }
-    const json = JSON.parse(fs.readFileSync("./roles.json", "utf-8"));
-    const roles = json[json.length - 1];
-    const cache = Client.guilds.cache
-        .get(process.env.DISCORD_SERVER_ID)
-        .members.cache.forEach((user) => {
-            if (users.has(user.user.id) && isNaN(time) != true) {
-                if (punishedUsers.includes(user.user.username)) {
-                    console.log(`${user.user.username} już nie żyż`);
-                    Client.channels.cache
-                        .get(process.env.DISCORD_COMMAND_CHANNEL)
-                        .send(`${user.user.username} już nie żyż`);
-                    return user;
-                } else {
-                    user.roles.add(
-                        user.guild.roles.cache.find(
-                            (r) => r.name == process.env.PUNISHMENT_ROLE
-                        )
-                    );
-                    punishedUsers = [...punishedUsers, user.user.username];
-                    fs.writeFileSync(
-                        "./punishedUsers.json",
-                        JSON.stringify(punishedUsers)
-                    );
-                    Client.channels.cache
-                        .get(process.env.DISCORD_COMMAND_CHANNEL)
-                        .send(
-                            `${
-                                user.user.username
-                            } is abonished to the depths of hell 2.0 for ${parseInt(
-                                time
-                            )} minutes`
-                        );
-                    setTimeout(async () => {
-                        console.log("------------\n zwalnianie użytkownika \n");
-                        user.roles.remove(
-                            user.guild.roles.cache.find(
-                                (r) => r.name == process.env.PUNISHMENT_ROLE
-                            )
-                        );
-                        punishedUsers = punishedUsers.filter((o) => {
-                            if (o != user.user.username) return user;
-                        });
-                        fs.writeFileSync(
-                            "./punishedUsers.json",
-                            JSON.stringify(punishedUsers)
-                        );
-                        console.log(
-                            punishedUsers.filter((o) => {
-                                if (o != user.user.username) return user;
-                            })
-                        );
-                    }, parseInt(time) * 1000 * 60);
-                }
+    const json = JSON.parse(fs.readFileSync("./roles.json", "utf-8"))
+    const roles = json[json.length - 1]
+    const cache = Client.guilds.cache.get(process.env.DISCORD_SERVER_ID).members.cache.forEach((user) => {
+        if (users.has(user.user.id) && isNaN(time) != true) {
+            if (punishedUsers.includes(user.user.username)) {
+                console.log(`${user.user.username} już nie żyż`)
+                Client.channels.cache.get(process.env.DISCORD_COMMAND_CHANNEL).send(`${user.user.username} już nie żyż`)
+                return user
             } else {
-                console.log("sth went wrong");
+                user.roles.add(user.guild.roles.cache.find((r) => r.name == process.env.PUNISHMENT_ROLE))
+                punishedUsers = [...punishedUsers, user.user.username]
+                fs.writeFileSync("./punishedUsers.json", JSON.stringify(punishedUsers))
+                Client.channels.cache
+                    .get(process.env.DISCORD_COMMAND_CHANNEL)
+                    .send(`${user.user.username} is abonished to the depths of hell 2.0 for ${parseInt(time)} minutes`)
+                setTimeout(async () => {
+                    console.log("------------\n zwalnianie użytkownika \n")
+                    punishedUsers = punishedUsers.filter((o) => {
+                        if (o != user.user.username) return user
+                    })
+                    fs.writeFileSync("./punishedUsers.json", JSON.stringify(punishedUsers))
+                    console.log(
+                        punishedUsers.filter((o) => {
+                            if (o != user.user.username) return user
+                        })
+                    )
+                    let isUserOnServer = false
+                    Client.guilds.cache.get(process.env.DISCORD_SERVER_ID).members.cache.forEach((newUsers) => {
+                        if (newUsers.user.id == user.user.id) isUserOnServer = true
+                    })
+                    if (isUserOnServer) {
+                        user.roles.remove(user.guild.roles.cache.find((r) => r.name == process.env.PUNISHMENT_ROLE))
+                    } else {
+                        console.log(`zwalnianie użytkownika po pliku`)
+                        let savedRoles = JSON.parse(fs.readFileSync("./roles.json", "utf-8"))
+                        savedRoles.map((o) => {
+                            if (o.clientId == user.user.id) {
+                                console.log(o.name)
+                                o.roles = o.roles.filter((role) => role != process.env.PUNISHMENT_ROLE)
+                            }
+                        })
+                        fs.writeFileSync("./roles.json", JSON.stringify(savedRoles))
+                    }
+                }, parseInt(time) * 1000 * 60)
             }
-        });
-    console.log(punishedUsers);
-};
+        } else {
+            console.log("sth went wrong")
+        }
+    })
+    console.log(punishedUsers)
+}
