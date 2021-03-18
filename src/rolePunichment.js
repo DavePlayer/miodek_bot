ac92b30d11c3643f8d403cfe62a1a9b550ed9142
DavePlayer <daveplayer2001@gmail.com>
2021-03-19 00:03:03 +0100

    fixed i hope all bugs including promises from deleting and adding roles

--- src/rolePunichment.js
+++ src/rolePunichment.js
@@ -4,11 +4,13 @@ class lastJudgment {
     constructor() {
         this.doomed = null
         this.roles = null
+        this.user = null
     }
 
     readDoomed() {
         this.doomed = JSON.parse(fs.readFileSync("./punishedUsers.json", "utf-8"))
-        this.roles = JSON.parse(fs.readFileSync("./roles.json", "utf-8"))[-1]
+        const { roles } = JSON.parse(fs.readFileSync("./roles.json", "utf-8")).pop()
+        this.roles = roles
     }
 
     saveDoomed(humanData) {
@@ -21,73 +23,142 @@ class lastJudgment {
         this.readDoomed()
         // Goes for every user on server and checks if user is still on server
         Client.guilds.cache.get(process.env.DISCORD_SERVER_ID).members.cache.forEach((user) => {
-            if (users.has(user.user.id) && isNaN(time) != true) {
-                if (this.doomed.some((o) => o.id == user.user.id)) {
+            this.user = user
+            if (users.has(this.user.user.id) && isNaN(time) != true) {
+                if (this.doomed.some((o) => o.id == this.user.user.id)) {
                     console.log(`${user.user.username} już nie żyż`)
                     Client.channels.cache
                         .get(process.env.DISCORD_COMMAND_CHANNEL)
-                        .send(`${user.user.username} już nie żyż`)
-                    return user
+                        .send(`${this.user.user.username} już nie żyż`)
+                    return this.user
                 } else {
                     //delete and save to file remaining roles that user had
-                    const userData = { id: user.user.id, roles: user.roles.member._roles }
+                    const userData = { id: this.user.user.id, roles: this.user.roles.member._roles }
                     this.saveDoomed(userData)
-                    user.roles.member.roles.remove(userData.roles)
+                    user.roles
+                        .remove(userData.roles)
+                        .then(() => {
+                            // add punishment role
+                            user.roles
+                                .add(user.guild.roles.cache.find((r) => r.name == process.env.PUNISHMENT_ROLE && r))
+                                .then(() => {
+                                    console.log(this.doomed)
 
-                    // add punishment role
-                    user.roles.add(user.guild.roles.cache.find((r) => r.name == process.env.PUNISHMENT_ROLE))
+                                    Client.channels.cache
+                                        .get(process.env.DISCORD_COMMAND_CHANNEL)
+                                        .send(
+                                            `${
+                                                user.user.username
+                                            } is abonished to the depths of hell 2.0 for ${parseFloat(time)} minutes`
+                                        )
+                                    setTimeout(async () => {
+                                        console.log("------------\n zwalnianie użytkownika \n")
+                                        console.log(this.doomed)
+                                        this.readDoomed()
 
-                    console.log(this.doomed)
+                                        //checking if user is still on server
+                                        let isUserOnServer = false
+                                        Client.guilds.cache
+                                            .get(process.env.DISCORD_SERVER_ID)
+                                            .members.cache.some((newUsers) => {
+                                                if (newUsers.user.id == user.user.id) {
+                                                    isUserOnServer = true
+                                                    return 1
+                                                }
+                                            })
 
-                    Client.channels.cache
-                        .get(process.env.DISCORD_COMMAND_CHANNEL)
-                        .send(
-                            `${user.user.username} is abonished to the depths of hell 2.0 for ${parseInt(time)} minutes`
-                        )
-                    setTimeout(async () => {
-                        console.log("------------\n zwalnianie użytkownika \n")
-                        console.log(this.doomed)
+                                        if (isUserOnServer) {
+                                            // if user is on serwer then remove punishment role from him
+                                            this.doomed.some((o) => {
+                                                console.log("xdddddddddddddddddddddddddddd")
+                                                if (o.id == user.user.id) {
+                                                    console.log(`DOOMED FOUND: ${o.roles}`)
+                                                    user.roles.member.roles
+                                                        .add(o.roles)
+                                                        .then((afterUser) => {
+                                                            //after add ing this  console log it started working
+                                                            /*console.log(
+                                                                user.guild.roles.cache.find(
+                                                                    (r) => r.name == process.env.PUNISHMENT_ROLE && r
+                                                                )
+                                                            )*/
+                                                            afterUser.roles
+                                                                .remove(
+                                                                    user.guild.roles.cache.find(
+                                                                        (r) =>
+                                                                            r.name == process.env.PUNISHMENT_ROLE && r
+                                                                    )
+                                                                )
+                                                                .then(() => {
+                                                                    this.doomed = this.doomed.filter((o) => {
+                                                                        if (o.id != user.user.id) return user
+                                                                    })
+                                                                    fs.writeFileSync(
+                                                                        "./punishedUsers.json",
+                                                                        JSON.stringify(this.doomed)
+                                                                    )
+                                                                })
+                                                                .catch((err) => console.log(err))
+                                                            return 1
+                                                        })
+                                                        .catch((err) => console.log(err))
+                                                }
+                                            })
+                                        } else {
+                                            console.log(`zwalnianie użytkownika po pliku`)
+                                            let savedRoles = JSON.parse(fs.readFileSync("./roles.json", "utf-8"))
+                                            /*savedRoles = [
+                                                ...savedRoles.slice(0, savedRoles.length - 1),
 
-                        //checking if user is still on server
-                        let isUserOnServer = false
-                        Client.guilds.cache.get(process.env.DISCORD_SERVER_ID).members.cache.some((newUsers) => {
-                            if (newUsers.user.id == user.user.id) {
-                                isUserOnServer = true
-                                return 1
-                            }
+                                                {
+                                                    name: user.user.username,
+                                                    clientId: user.user.id,
+                                                    roles: this.roles
+                                                        .filter((role) => {
+                                                            if (
+                                                                role.id ==
+                                                                this.doomed.some((savedRole) => savedRole == role.id)
+                                                            )
+                                                                return role.name
+                                                        })
+                                                        .map((o) => o.name),
+                                                    user: user._roles,
+                                                },
+                                                savedRoles.pop(),
+                                            ]*/
+                                            console.log(savedRoles)
+                                            savedRoles.some((o) => {
+                                                //pod dawidem
+                                                if (o.clientId == user.user.id) {
+                                                    console.log("found doomed")
+                                                    o.roles = o.roles.filter(
+                                                        (role) => role != process.env.PUNISHMENT_ROLE && role
+                                                    )
+                                                    console.log(o.roles)
+                                                    o.roles = this.roles.map((role) => {
+                                                        const [data] = this.doomed.filter((doom) => {
+                                                            if (doom.id == user.user.id && doom.roles) return doom.roles
+                                                        })
+                                                        console.log("doooomed", data)
+                                                        if (data.roles.includes(role.id)) return role.name
+                                                    })
+                                                    o.roles = o.roles.filter((ll) => ll)
+                                                    return 1
+                                                }
+                                            })
+                                            console.log(savedRoles)
+                                            fs.writeFileSync("./roles.json", JSON.stringify(savedRoles))
+                                            this.doomed = this.doomed.filter((o) => {
+                                                if (o.id != user.user.id) return user
+                                            })
+                                            fs.writeFileSync("./punishedUsers.json", JSON.stringify(this.doomed))
+                                        }
+                                        // releasing punishment role from saved roles file and returning old ones
+                                    }, parseFloat(time) * 1000 * 60)
+                                })
+                                .catch((err) => console.log(err))
                         })
-                        if (isUserOnServer) {
-                            // if user is on serwer then remove punishment role from him
-                            user.roles.remove(user.guild.roles.cache.find((r) => r.name == process.env.PUNISHMENT_ROLE))
-                            this.readDoomed()
-                            this.doomed.some((o) => {
-                                console.log("xdddddddddddddddddddddddddddd")
-                                if (o.id == user.user.id) {
-                                    console.log(`DOOMED FOUND: ${o.roles}`)
-                                    user.roles.member.roles.add(o.roles)
-                                }
-                            })
-                        } else {
-                            // releasing punishment role from saved roles file and returning old ones
-                            console.log(`zwalnianie użytkownika po pliku`)
-                            let savedRoles = JSON.parse(fs.readFileSync("./roles.json", "utf-8"))
-                            savedRoles.some((o) => {
-                                if (o.clientId == user.user.id) {
-                                    console.log(`------------------\nroles inf ifle: ${o.roles}\n--------------`)
-                                    o.roles = o.roles.filter((role) => role != process.env.PUNISHMENT_ROLE)
-                                    //to be continued...
-                                    //o.user = userData.roles
-                                    return 1
-                                }
-                            })
-                            fs.writeFileSync("./roles.json", JSON.stringify(savedRoles))
-                        }
-
-                        this.doomed = this.doomed.filter((o) => {
-                            if (o.id != user.user.id) return user
-                        })
-                        fs.writeFileSync("./punishedUsers.json", JSON.stringify(this.doomed))
-                    }, parseFloat(time) * 1000 * 60)
+                        .catch((err) => console.log(err))
                 }
             } else {
                 console.log("not this user or time invalid")
